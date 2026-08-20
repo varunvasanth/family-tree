e81a4f3d15078129733ff475d47723ca6661f92b
varunvasanth <95974676+varunvasanth@users.noreply.github.com>
2026-08-20 10:21:21 -0400

    Fix

--- idk.js
+++ idk.js
@@ -1,5 +1,5 @@
 let a = 1;
 function buttonClicked() {
     a = a+1;
-    document.getElementById("show").innertext = "you have added "+a+"people"
+    document.getElementById("show").innertext = "you have added "+a+"people";
 };
